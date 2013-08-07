248f6fdc1e8d6f6d02d94e4ca4d467abbc3a3e45
Jeff Upton <jeffupt@gmail.com>
2013-08-06 22:38:57 -0500

    Add random

---
+++ include/oblivion/core/random.h
@@ -0,0 +1,75 @@
+/* Copyright (c) 2013 Oblivion Software */
+
+#ifndef _OBLIVION_CORE_RANDOM_H_
+#define _OBLIVION_CORE_RANDOM_H_
+
+#include <oblivion/core/base.h>
+#include <oblivion/core/types.h>
+
+namespace oblivion {
+
+	/**
+	 * Random number generator.
+	 */
+	class OB_CORE_API Random {
+
+	public:
+
+		/**
+		 * Initializes the random number generator seeded with the current time.
+		 */
+		Random();
+
+		/**
+		 * Initializes the random number generator with a seed.
+		 * @param seed The random seed.
+		 */
+		Random(int32 seed);
+
+		/**
+		 * Gets the next integer in the random sequence. [1 - 2^31)
+		 * @return A random integer.
+		 */
+		int32 next();
+
+		/**
+		 * Gets the next integer in the range [0, rangeEnd).
+		 * @param rangeEnd The exclusive maximum of the range.
+		 * @return A random integer in the range [0, rangeEnd).
+		 */
+		int32 nextRange(int32 rangeEnd);
+
+		/**
+		 * Gets a random bool value.
+		 * @return True or false, randomly.
+		 */
+		bool nextBool();
+
+		/**
+		 * Gets the next real value in the range [0, 1]
+		 * @return A random real in the range [0, 1]
+		 */
+		real32 nextReal();
+
+	private:
+
+		/**
+		 * Initializes the random number generator with the specified seed.
+		 * @param seed The random seed.
+		 */
+		void init(int32 seed);
+
+		/**
+		 * Generates the numbers for the mersenne twister.
+		 */
+		void generateNumbers();
+
+		int32 mt_[624];
+
+		int32 index_;
+
+	};
+
+}
+
+#endif /* _OBLIVION_CORE_RANDOM_H_ */
---
+++ src/oblivion/core/random.cpp
@@ -0,0 +1,85 @@
+/* Copyright (c) 2013 Oblivion Software */
+
+#include <oblivion/core/random.h>
+
+#include <ctime>
+#include <limits>
+
+namespace oblivion {
+
+/*****************************************************************************/
+
+Random::Random() {
+	auto seed = static_cast<int32>(time(nullptr));
+	init(seed);	
+}
+
+/*****************************************************************************/
+
+Random::Random(int32 seed) {
+	init(seed);
+}
+
+/*****************************************************************************/
+
+int32 Random::next() {
+    if (index_ == 0) {
+        generateNumbers();
+    }
+
+    int32 y = mt_[index_];
+    y = y ^ (y >> 11);
+    y = y ^ ((y << 7) & 0x9d2c5680);
+    y = y ^ ((y << 15) & 0xefc60000);
+    y = y ^ (y >> 18);
+
+    index_ = (index_ + 1) % 624;
+
+    return y;
+}
+
+/*****************************************************************************/
+
+int32 Random::nextRange(int32 rangeEnd) {
+	return next() % rangeEnd;
+}
+
+/*****************************************************************************/
+
+bool Random::nextBool() {
+	return (next() % 2) == 0;
+}
+
+/*****************************************************************************/
+
+real32 Random::nextReal() {
+	return static_cast<real32>(next() / std::numeric_limits<int32>::max());
+}
+
+/*****************************************************************************/
+
+void Random::init(int32 seed) {
+	index_ = 0;
+	mt_[0] = seed;
+
+	for (int i = 1; i < 624; ++i) {
+		mt_[i] = (0x6c078965 * (mt_[i - 1] ^ (mt_[i - 1] >> 30))) + i;
+	}
+}
+
+/*****************************************************************************/
+
+void Random::generateNumbers() {
+	for (int32 i = 0; i < 624; ++i) {
+        int32 y = (mt_[i] & 0x80000000) + (mt_[(i + 1) % 624] & 0x7fffffff);
+        mt_[i] = mt_[(i + 397) % 624] ^ (y >> 1);
+        
+        if ((y % 2) != 0) {
+            mt_[i] = mt_[i] ^ 0x9908b0df;
+        }
+    }
+}
+
+/*****************************************************************************/
+
+}
