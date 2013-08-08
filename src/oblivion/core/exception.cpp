7bcf416b591b0b0048f0f29a804d059904f7474e
Jeff Upton <jeffupt@gmail.com>
2013-08-07 21:46:11 -0500

    Add some sample static errors

--- src/oblivion/core/exception.cpp
+++ src/oblivion/core/exception.cpp
@@ -23,6 +23,10 @@ Exception::Exception(const std::string& message, const char* file, const char* f
 /*****************************************************************************/
 
 int32 Exception::line() const {
+     int a;
+     int b = 3 + a;
+     std::string bad = "asdf" + 'c';
+
     return line_;
 }
 
